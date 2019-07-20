d101005a28e523e131258c97a08d82600c6094fd
Barış Yılmaz <53113765+barisylmzz@users.noreply.github.com>
2019-07-21 01:24:35 +0300

    firstt commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDescription);
+            this.groupBox1.Controls.Add(this.txtCategoryName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 162);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kategori Ekleme Paneli";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(185, 91);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(184, 22);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Location = new System.Drawing.Point(185, 57);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(184, 22);
+            this.txtCategoryName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(52, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Açıklama:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı:";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(12, 168);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(388, 44);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.Text = "KAYDET";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(12, 218);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(388, 44);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(12, 268);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(388, 44);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(0, 325);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(412, 244);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 569);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(Properties.Settings.Default.Northwind);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            query = "Select CategoryID,CategoryName From Categories Order By CategoryID desc";
+            cmd = new SqlCommand(query, conn);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["CategoryID"] + "-" + reader["CategoryName"]);
+                //MessageBox.Show(reader[0].ToString());
+            }
+            conn.Close();
+
+        }
+        SqlDataReader reader;
+        string query;
+        SqlCommand cmd;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            query = "Select CategoryID,CategoryName,Description From Categories";
+            cmd = new SqlCommand(query, conn);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //listBox1.Items.Add(reader["CategoryID"] + "-" + reader["CategoryName"]);
+                if (listBox1.SelectedItem.ToString() == reader["CategoryID"] + "-" + reader["CategoryName"])
+                {
+                    txtCategoryName.Text = reader["CategoryName"].ToString();
+                    txtDescription.Text = reader["Description"].ToString();
+                }
+            }
+            conn.Close();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            int affectedRows = 0;
+            string kayit = "Insert Into Categories(CategoryName,Description) Values ('" + txtCategoryName.Text + "','" + txtDescription.Text + "')";
+            cmd = new SqlCommand(kayit, conn);
+            query = "Select CategoryID,CategoryName,Description From Categories Order By CategoryID desc";
+            SqlCommand cmd1 = new SqlCommand(query, conn);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                affectedRows = (int)cmd.ExecuteNonQuery();
+            }
+            reader = cmd1.ExecuteReader();
+            listBox1.Items.Clear();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["CategoryID"] + "-" + reader["CategoryName"]);
+            }
+            conn.Close();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int affectedRows = 0;
+            string guncelleme = "Update Categories Set CategoryName='" + txtCategoryName.Text + "',Description='" + txtDescription.Text + "' Where Convert(nvarchar,CategoryID)+'-'+CategoryName='" + listBox1.SelectedItem.ToString() + "'";
+            cmd = new SqlCommand(guncelleme, conn);
+            query = "Select CategoryID,CategoryName,Description From Categories Order By CategoryID desc";
+            SqlCommand cmd1 = new SqlCommand(query, conn);
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                affectedRows = cmd.ExecuteNonQuery();
+            }
+            reader = cmd1.ExecuteReader();
+            listBox1.Items.Clear();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["CategoryID"] + "-" + reader["CategoryName"]);
+               
+            }
+            conn.Close();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int affectedRows = 0;
+            string silme = "Delete From Categories Where Convert(nvarchar,CategoryID)+'-'+CategoryName='" + listBox1.SelectedItem.ToString() + "'";
+            cmd = new SqlCommand(silme, conn);
+            query = "Select CategoryID,CategoryName,Description From Categories Order By CategoryID desc";
+            SqlCommand cmd1 = new SqlCommand(query, conn);
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                affectedRows = cmd.ExecuteNonQuery();
+            }
+            reader = cmd1.ExecuteReader();
+            listBox1.Items.Clear();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["CategoryID"] + "-" + reader["CategoryName"]);
+
+            }
+            conn.Close();
+        }
+    }
+}
